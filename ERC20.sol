993cf8b61ea7c0d7dd3e53da6b70ee9a4a5c869b
Angel Flores <aflores@navicu.com>
2018-04-05 14:47:09 -0400

    Primer commit, contratos ERC20 de prueba

---
+++ ERC20.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+interface ERC20 {
+  function totalSupply() constant returns (uint256 totalSupply);
+  function balanceOf(address _owner) constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  function approve(address _spender, uint256 _value) returns (bool success);
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
